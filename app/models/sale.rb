09e1b94fa909c1f14590fb7d1103f0047a70ddf4
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-14 20:59:16 -0300

    Finish Sale model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,8 @@
 class Sale < ApplicationRecord
   belongs_to :company_sale
+
+  validates :purchaser_name, presence: true
+  validates :item_description, presence: true
+  validates :item_price, presence: true, numericality: true
+  validates :purchase_count, presence: true, numericality: true
 end
--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Sale, type: :model do
-  describe '#company_sales' do
-    it { is_expected.to belong_to(:company_sale) }
+  describe '.company_sale' do
+    it { is_expected.to belong_to(:company_sale).required }
+  end
+
+  describe '.purchaser_name' do
+    it { is_expected.to validate_presence_of :purchaser_name }
+  end
+
+  describe '.item_price' do
+    it { is_expected.to validate_presence_of :item_price }
+    it { is_expected.to validate_numericality_of(:item_price) }
+  end
+
+  describe '.purchase_count' do
+    it { is_expected.to validate_presence_of :purchase_count }
+    it { is_expected.to validate_numericality_of(:item_price) }
   end
 end
