4a6d06d396290c028b07f5d22019effa821c4604
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-14 17:16:00 -0300

    Add CompanySale validations

--- app/models/company_sale.rb
+++ app/models/company_sale.rb
@@ -1,3 +1,6 @@
 class CompanySale < ApplicationRecord
   has_many :sales, inverse_of: :company_sale, dependent: :destroy
+
+  validates :imported_at, presence: true
+  validates :total_gross_income, presence: true
 end
--- spec/models/company_sale_spec.rb
+++ spec/models/company_sale_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CompanySale, type: :model do
-  describe '#sales' do
+  describe '.sales' do
     it { is_expected.to have_many(:sales).inverse_of(:company_sale).dependent(:destroy) }
+  end
+
+  describe '.imported_at' do
+    it { is_expected.to validate_presence_of :imported_at }
+  end
+
+  describe '.total_gross_income' do
+    it { is_expected.to validate_presence_of :total_gross_income }
   end
 end
