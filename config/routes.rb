1ae8c64422a70f9a0e78f5ce6667521b218fefda
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-15 12:06:22 -0300

    Remove unnecessary scaffolding code

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants
-      resources :sales
       resources :company_sales, except: [:update]
     end
   end
