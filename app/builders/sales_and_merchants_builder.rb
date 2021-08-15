b3aab1fa1235ea0e2673fd3e95330bd54f6d3206
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-15 18:22:03 -0300

    WIP SalesBuilder

---
+++ app/builders/sales_and_merchants_builder.rb
@@ -0,0 +1,26 @@
+class SalesAndMerchantsBuilder < ApplicationBuilder
+  def initialize(sales_and_merchants_to_build)
+    @sales_and_merchants_to_build = sales_and_merchants_to_build
+  end
+
+  def build
+    @sales_and_merchants_to_build.map do |sale_and_merchant_to_build|
+      Sale.new(
+        purchaser_name: sale_and_merchant_to_build['purchaser name'],
+        item_description: sale_and_merchant_to_build['item description'],
+        item_price: sale_and_merchant_to_build['item price'],
+        purchase_count: sale_and_merchant_to_build['purchase count'],
+        merchant: build_merchant(sale_and_merchant_to_build)
+      )
+    end
+  end
+
+  private
+
+  def build_merchant(merchant_to_build)
+    Merchant.new(
+      name: merchant_to_build['merchant name'],
+      address: merchant_to_build['merchant address']
+    )
+  end
+end
---
+++ spec/builders/sales_and_merchants_builder_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe SalesAndMerchantsBuilder do
+  subject(:sales) { described_class.build(sales_to_build) }
+
+  let(:sales_to_build) do
+    [
+      {
+        'purchaser name': 'João Silva',
+        'item description': 'R$10 off R$20 of food',
+        'item price': '10.0',
+        'purchase count': '2',
+        'merchant address': '987 Fake St',
+        'merchant name': "Bob's Pizza"
+      }.with_indifferent_access,
+      {
+        'purchaser name': 'Amy Pond',
+        'item description': 'R$30 of awesome for R$10',
+        'item price': '20.0',
+        'purchase count': '5',
+        'merchant address': '456 Unreal Rd',
+        'merchant name': "Tom's Awesome Shop"
+      }.with_indifferent_access
+    ]
+  end
+
+  describe '#build' do
+    it 'retuns a list of instances of Sales' do
+      expect(sales.first).to be_an_instance_of Sale
+    end
+
+    describe '.purchaser_name' do
+      it 'returns the purchaser name' do
+        expect(sales.first.purchaser_name).to eql 'João Silva'
+        expect(sales.second.purchaser_name).to eql 'Amy Pond'
+      end
+    end
+
+    describe '.item_description' do
+      it 'returns the item description' do
+        expect(sales.first.item_description).to eql 'R$10 off R$20 of food'
+        expect(sales.second.item_description).to eql 'R$30 of awesome for R$10'
+      end
+    end
+
+    describe '.item_price' do
+      it 'returns the item price' do
+        expect(sales.first.item_price).to be 10.0
+        expect(sales.second.item_price).to be 20.0
+      end
+    end
+
+    describe '.purchase_count' do
+      it 'returns the purchase count' do
+        expect(sales.first.purchase_count).to be 2
+        expect(sales.second.purchase_count).to be 5
+      end
+    end
+
+    describe '.merchant' do
+      xit 'does not create a merchant if he already exists' do
+        create(:merchant, name: '')
+      end
+
+      it '.address returns the purchase merchant address' do
+        expect(sales.first.merchant.address).to eql '987 Fake St'
+        expect(sales.second.merchant.address).to eql '456 Unreal Rd'
+      end
+
+      it '.name returns the purchase merchante name' do
+        expect(sales.first.merchant.name).to eql "Bob's Pizza"
+        expect(sales.second.merchant.name).to eql "Tom's Awesome Shop"
+      end
+    end
+  end
+end
