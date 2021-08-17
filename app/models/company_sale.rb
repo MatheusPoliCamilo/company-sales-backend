5a30afacec80ba4014106962f784daf73bc26964
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-17 01:36:07 -0300

    Update CompanySale total_gross_income when updated

--- app/models/company_sale.rb
+++ app/models/company_sale.rb
@@ -2,4 +2,12 @@ class CompanySale < ApplicationRecord
   has_many :sales, inverse_of: :company_sale, dependent: :destroy
 
   validates :imported_at, presence: true
+
+  before_save :update_total_gross_income
+
+  private
+
+  def update_total_gross_income
+    self.total_gross_income = sales.sum(&:item_price)
+  end
 end
--- spec/models/company_sale_spec.rb
+++ spec/models/company_sale_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe CompanySale, type: :model do
   describe '.imported_at' do
     it { is_expected.to validate_presence_of :imported_at }
   end
+
+  describe '.total_gross_income' do
+    it 'is updated with the total sales price when saved' do
+      sales = [build(:sale, item_price: 10.15), build(:sale, item_price: 10.15)]
+      company_sale = build(:company_sale, total_gross_income: 0, sales: sales)
+
+      company_sale.save
+
+      expect(company_sale.total_gross_income).to eq 20.30
+    end
+  end
 end
